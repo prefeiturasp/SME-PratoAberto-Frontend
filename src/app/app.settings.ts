7539c9176920ec318670a78764a0f12c299a38dd
Vitor Hirota <vitor.hirota@gmail.com>
2017-11-01 02:58:37 -0200

    atualização apontamentos

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-  public static BASE_URL:string = "https://pratoaberto.tk/api";
+  public static BASE_URL:string = "https://pratoaberto.sme.prefeitura.sp.gov.br/api";
   public static SCHOOLS_ENDPOINT:string = "/escolas";
   public static SCHOOL_ENDPOINT:string = "/escola";
   public static SCHOOLS_DETAILS_ENDPOINT:string = "/assets/data/detalhes.json";
