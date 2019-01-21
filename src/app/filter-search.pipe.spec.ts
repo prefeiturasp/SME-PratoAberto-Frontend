189ddbd72bc2728a354d9d0307aad428c61627fd
Weslei Souza <weslei.souza@amcom.com.br>
2019-01-21 10:42:06 -0200

    Ajuste para push

---
+++ src/app/filter-search.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterSearchPipe } from './filter-search.pipe';
+
+describe('FilterSearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterSearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
