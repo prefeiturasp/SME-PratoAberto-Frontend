1b4a445d820417e2554d978ad6e72478c50041c7
Marcelo Maia <mmaia.cc@gmail.com>
2019-03-11 15:06:14 -0300

    link api correto para producao

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-  public static BASE_URL = 'http://10.49.16.10/api';
+  public static BASE_URL = 'https://pratoaberto.sme.prefeitura.sp.gov.br/api';
   public static SCHOOLS_ENDPOINT = '/escolas';
   public static SCHOOL_ENDPOINT = '/escola';
   public static SCHOOLS_DETAILS_ENDPOINT = '/assets/data/detalhes.json';
